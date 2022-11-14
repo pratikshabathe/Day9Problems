39d9924ae3e75d74e6cd6ecb409512dfc67dcc24
pratikshabathe <pratikshabathe11@gmail.com>
2022-11-14 07:35:41 +0530

    create new branch

---
+++ EmpWageProgram.java
@@ -0,0 +1,25 @@
+package bl.com.day9;
+
+import java.util.Random;
+
+public class  EmpWageProgram  {
+	
+	public static void main(String[] args) {
+		System.out.println("--Welcome to Employee Wage Computation Program--\n");
+		
+	   String attendanceString =  EmpWageProgram.checkAttendance();
+	    System.out.println(attendanceString);
+	    checkAttendance() ;
+
+	}
+
+	  private static String checkAttendance() {
+	    Random random = new Random();
+	    boolean randomBool = random.nextBoolean();
+
+	    if (randomBool) return "Employee is present.";
+	    
+	    else return "Employee is absent.";
+
+	}
+}
