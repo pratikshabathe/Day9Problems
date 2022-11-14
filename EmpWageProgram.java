205aa6282f5da1bfd7b8409b80f9794cb482cd2a
pratikshabathe <pratikshabathe11@gmail.com>
2022-11-14 07:45:12 +0530

    create new branch

--- EmpWageProgram.java
+++ EmpWageProgram.java
@@ -11,15 +11,35 @@ public class  EmpWageProgram  {
 	    System.out.println(attendanceString);
 	    checkAttendance() ;
 
+	    System.out.println("Employee's today wage :" +getDailyWage(attendanceString));
 	}
 
 	  private static String checkAttendance() {
 	    Random random = new Random();
 	    boolean randomBool = random.nextBoolean();
+	    
+		int randomNum = 1;
+		if(randomNum == 1) return "employee is Present full time";
+	    else if (randomNum == 2) return "employee is present part time";
 
 	    if (randomBool) return "Employee is present.";
-	    
 	    else return "Employee is absent.";
-
+	}
+	  
+	private static int getDailyWage(String str) {
+		
+		boolean isPresent = str.contains("present") || str.contains("present");
+		
+		byte empRatePerHr = 20;
+		byte hrsWorked = 0;
+		byte fullTimeHrs = 8;
+		byte partTimeHrs = 4;
+		
+		if(str.contains("full time")) hrsWorked = fullTimeHrs;
+		else if(str.contains("part time")) hrsWorked = partTimeHrs;
+		
+		//if (isPresent) hrsWorked = 8;
+		int dailyWage = empRatePerHr * hrsWorked;
+		return dailyWage;
 	}
 }
