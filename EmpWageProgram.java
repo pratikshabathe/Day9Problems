8a498b14360f886489293f12e0f70ed68d8dac40
pratikshabathe <pratikshabathe11@gmail.com>
2022-11-14 08:02:40 +0530

    create new branch

--- EmpWageProgram.java
+++ EmpWageProgram.java
@@ -2,50 +2,64 @@ package bl.com.day9;
 
 import java.util.Random;
 
-public class  EmpWageProgram  {
-	
-	public static void main(String[] args) {
-		System.out.println("--Welcome to Employee Wage Computation Program--\n");
-		
-	   String attendanceString =  EmpWageProgram.checkAttendance();
-	    System.out.println(attendanceString);
-	    checkAttendance() ;
-	    
-	    System.out.println("Employee's today's wage :" + getDailyWage(attendanceString));
+public class EmpWageProgram{
 
-	}
+	  public static void main(String[] args) {
+	    System.out.println("--Welcome to Employee Wage Computation Program--\n");
+
+	    System.out.println("Employee's wage for this month: " + getMonthlyWage() + "$");
+
+	  }
 
 	  private static String checkAttendance() {
 	    Random random = new Random();
-	    int randomBool = random.nextInt();
+	    int randomNum = random.nextInt(0);
 
-	    int randomNum = 0;
-		switch(randomNum) {
+	    switch (randomNum) {
 	    case 1:
-	    	return "employee is present full time";
-	    
+	      return "employee is present full time";
+
 	    case 2:
-	    	return "employee is present part time";
-	    	
-	    default :
-	    	return " employee is absent";
-	    			
+	      return "employee is present part time";
+
+	    default:
+	      return "employee is absent";
 	    }
 
-	}
-	  
-	  private static int getDailyWage(String str) {
-		  
-		  Byte empRatePerHr = 20;
-		  Byte hrsWorked = 0;
-		  Byte fullTimeHrs = 8;
-		  Byte partTimeHrs = 4;
-		  
-		  if(str.contains("full time")) hrsWorked = fullTimeHrs;
-		  else if(str.contains("part time")) hrsWorked = partTimeHrs;
-		  
-		  int dailyWage = empRatePerHr*hrsWorked;
-		  return dailyWage;
-		  
 	  }
+
+	  private static int getDailyWage() {
+
+	   Byte empRatePerHr = 20;
+	   Byte hrsWorked = 0;
+	   Byte fullTimeHrs = 8;   //assumed
+	   Byte partTimeHrs = 4;   //assumed
+
+	   String str = checkAttendance();
+
+	   if (str.contains("full time")) hrsWorked = fullTimeHrs;
+	   else if (str.contains("part time")) hrsWorked = partTimeHrs;
+
+	   int dailyWage = empRatePerHr * hrsWorked;
+	   return dailyWage;
+	 }
+
+
+	 private static int getMonthlyWage() {
+
+	  int monthlyWage = 0;
+	  short daysWorked;
+
+	  for (daysWorked = 0; daysWorked < 20;) {
+
+	    int dailyWage = getDailyWage(); //getting employee's daily wage for each day
+
+	    if (dailyWage > 0) {            //checking if the employee is present i.e. has dailyWage>0
+	      monthlyWage += dailyWage;
+	      daysWorked++;                 //only increasing number of days worked if the employee HAS worked, either full time or part time
+	      }
+	    }
+
+	    return monthlyWage;
+	 }
 }
