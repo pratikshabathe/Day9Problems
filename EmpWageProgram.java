8366d7f47e4bb7b309b9c936e466fca1534479b3
pratikshabathe <pratikshabathe11@gmail.com>
2022-11-14 07:49:42 +0530

    create new branch

--- EmpWageProgram.java
+++ EmpWageProgram.java
@@ -10,36 +10,42 @@ public class  EmpWageProgram  {
 	   String attendanceString =  EmpWageProgram.checkAttendance();
 	    System.out.println(attendanceString);
 	    checkAttendance() ;
+	    
+	    System.out.println("Employee's today's wage :" + getDailyWage(attendanceString));
 
-	    System.out.println("Employee's today wage :" +getDailyWage(attendanceString));
 	}
 
 	  private static String checkAttendance() {
 	    Random random = new Random();
-	    boolean randomBool = random.nextBoolean();
-	    
-		int randomNum = 1;
-		if(randomNum == 1) return "employee is Present full time";
-	    else if (randomNum == 2) return "employee is present part time";
+	    int randomBool = random.nextInt();
 
-	    if (randomBool) return "Employee is present.";
-	    else return "Employee is absent.";
+	    int randomNum = 0;
+		switch(randomNum) {
+	    case 1:
+	    	return "employee is present full time";
+	    
+	    case 2:
+	    	return "employee is present part time";
+	    	
+	    default :
+	    	return " employee is absent";
+	    			
+	    }
+
 	}
 	  
-	private static int getDailyWage(String str) {
-		
-		boolean isPresent = str.contains("present") || str.contains("present");
-		
-		byte empRatePerHr = 20;
-		byte hrsWorked = 0;
-		byte fullTimeHrs = 8;
-		byte partTimeHrs = 4;
-		
-		if(str.contains("full time")) hrsWorked = fullTimeHrs;
-		else if(str.contains("part time")) hrsWorked = partTimeHrs;
-		
-		//if (isPresent) hrsWorked = 8;
-		int dailyWage = empRatePerHr * hrsWorked;
-		return dailyWage;
-	}
+	  private static int getDailyWage(String str) {
+		  
+		  Byte empRatePerHr = 20;
+		  Byte hrsWorked = 0;
+		  Byte fullTimeHrs = 8;
+		  Byte partTimeHrs = 4;
+		  
+		  if(str.contains("full time")) hrsWorked = fullTimeHrs;
+		  else if(str.contains("part time")) hrsWorked = partTimeHrs;
+		  
+		  int dailyWage = empRatePerHr*hrsWorked;
+		  return dailyWage;
+		  
+	  }
 }
